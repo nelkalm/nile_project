bd1c74223f00d97cbabb6ca62f5b4e1a724c0c9c
Nelson Lu <nelkalmlu@gmail.com>
2022-06-03 20:17:34 -0700

    Final commit

--- script.py
+++ script.py
@@ -2,6 +2,8 @@ from nile import get_distance, format_price, SHIPPING_PRICES
 from test import test_function
 
 # Define calculate_shipping_cost() here:
+
+
 def calculate_shipping_cost(from_coords, to_coords, shipping_type='Overnight'):
     from_lat, from_long = from_coords
     to_lat, to_long = to_coords
@@ -10,10 +12,13 @@ def calculate_shipping_cost(from_coords, to_coords, shipping_type='Overnight'):
     price = distance * shipping_rate
     return format_price(price)
 
-# Test the function by calling 
+
+# Test the function by calling
 test_function(calculate_shipping_cost)
 
 # Define calculate_driver_cost() here
+
+
 def calculate_driver_cost(distance, *drivers):
     cheapest_driver = None
     cheapest_driver_price = None
@@ -28,11 +33,21 @@ def calculate_driver_cost(distance, *drivers):
             cheapest_driver_price = price_for_driver
     return cheapest_driver_price, cheapest_driver
 
-# Test the function by calling 
-# test_function(calculate_driver_cost)
+
+# Test the function by calling
+test_function(calculate_driver_cost)
 
 # Define calculate_money_made() here
 
 
-# Test the function by calling 
-# test_function(calculate_money_made)
+def calculate_money_made(**any_trips):
+    trips = any_trips
+    total_money_made = 0
+    for trip_id, trip in trips.items():
+        trip_revenue = trip.cost - trip.driver.cost
+        total_money_made += trip_revenue
+    return total_money_made
+
+
+# Test the function by calling
+test_function(calculate_money_made)
